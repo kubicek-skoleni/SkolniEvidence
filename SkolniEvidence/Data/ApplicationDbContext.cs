75828c32e15793f59d2fc29bd9aa5b603f374098
Kubicek <82667968+kubicek-skoleni@users.noreply.github.com>
2025-08-21 11:39:33 +0200

    db

--- SkolniEvidence/Data/ApplicationDbContext.cs
+++ SkolniEvidence/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SkolniEvidence.Model;
 
 namespace SkolniEvidence.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Group> Groups { get; set; }
     }
 }
